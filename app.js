b33ddc3f2934aa45f2b8d2ca2cf423d83231f1c3
sun121 <sunxiaoyang0121@163.com>
2016-01-17 14:54:37 +0800

    页面布局

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/article', articles);
+app.use('/articles', articles);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
